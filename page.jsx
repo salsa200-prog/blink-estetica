dc0f8e607e6b64eb5f02e0a790ef8df483f2c805
salsa200-prog <claudiogabrielfk@gmail.com>
2026-02-05 22:49:21 -0300

    Remove root page.jsx

--- page.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{padding: 40}}>
-      <h1>Blink Estética Automotiva</h1>
-      <p>Site publicado com sucesso 🚀</p>
-    </main>
-  );
-}
