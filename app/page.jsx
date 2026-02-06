12999b2187d9096e649da830028fb03a0f614a4f
salsa200-prog <claudiogabrielfk@gmail.com>
2026-02-05 22:44:42 -0300

    Add app/page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Blink Estética Automotiva</h1>
+      <p>Site publicado com sucesso 🚀</p>
+    </main>
+  );
+}
