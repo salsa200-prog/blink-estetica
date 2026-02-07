397ce16c03add41410ea576270472b883146fbaa
salsa200-prog <claudiogabrielfk@gmail.com>
2026-02-06 23:27:11 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,14 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "BLINK Estética Automotiva",
+  description:
+    "Pintura, Polimento, Cristalização e Lanternagem na Barra da Tijuca - RJ",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   );
